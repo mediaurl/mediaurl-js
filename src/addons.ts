7648b048c824b46386745d0a1c99e71a66401144
Alex0007 <Alex0007lolpvp@gmail.com>
2019-12-12 04:24:33 +0500

    registerActionHandler

--- src/addons.ts
+++ src/addons.ts
@@ -1,7 +1,38 @@
+import { getServerValidators } from "@watchedcom/schema";
 import { WorkerAddon as WorkerAddonProps } from "@watchedcom/schema/dist/entities";
 
-export interface WorkerAddon {}
+type HandlerFn = (...args: any[]) => any;
+
+export interface IWorkerAddon {
+    registerActionHandler(action: string, handler: HandlerFn): void;
+}
+
+export class WorkerAddon implements IWorkerAddon {
+    constructor(private props: WorkerAddonProps) {}
+
+    public registerActionHandler() {}
+}
+
+/** Wrapper arount crazy untyped `@watched/schema` getServerValidators stuff */
+const validateWorkerAddonProps = (input: any): WorkerAddonProps => {
+    try {
+        const result: WorkerAddonProps = getServerValidators().models.addon(
+            input
+        );
+        return result;
+    } catch (error) {
+        // Actual error message is polluted with big json string that pollutes output
+        console.error(
+            `Addon validation failed.\nCheck out schema at https://github.com/watchedcom/schema/blob/master/schema.yaml`
+        );
+        throw new Error("Validation error");
+    }
+};
 
 export const createWorkerAddon = (
     props: Partial<WorkerAddonProps>
-): WorkerAddon => {};
+): WorkerAddon => {
+    const addonProps = validateWorkerAddonProps({ ...props, type: "worker" });
+    const addon = new WorkerAddon(addonProps);
+    return addon;
+};
