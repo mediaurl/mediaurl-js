eac4066195d3ca3384afb3f5692fea377282034b
Alex0007 <Alex0007lolpvp@gmail.com>
2019-12-12 05:58:59 +0500

    use error handler

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,38 @@
 import * as express from "express";
+import "express-async-errors";
 
 import { WorkerAddon } from "./addons";
+import { errorHandler } from "./error-handler";
 
-export interface ServeAddonOptions {}
+export interface ServeAddonOptions {
+    errorHandler: express.ErrorRequestHandler;
+}
 
-const defaultServeOpts: ServeAddonOptions = {};
+const defaultServeOpts: ServeAddonOptions = {
+    errorHandler
+};
+
+const _makeAddonRouter = (addon: WorkerAddon) => {
+    const router = express.Router();
+
+    router.get("/health", (req, res) => {
+        return res.send("OK");
+    });
+
+    router.post("/:action", async (req, res, next) => {
+        const { action } = req.params;
+        const resp = await addon.handleAction(action, {});
+        res.send({ action, resp });
+    });
+
+    return router;
+};
 
 export const generateAddonsRouter = (addons: WorkerAddon[]): express.Router => {
     const router = express.Router();
 
     addons.forEach(addon => {
-        const childRouter = express.Router();
-        const props = addon.getProps();
-
-        childRouter.get("/health", (req, res) => {
-            return res.send("OK");
-        });
-
-        router.use(`/${props.id}`, childRouter);
+        router.use(`/${addon.getProps().id}`, _makeAddonRouter(addon));
     });
 
     return router;
@@ -26,15 +41,17 @@ export const generateAddonsRouter = (addons: WorkerAddon[]): express.Router => {
 export const serveAddons = async (
     addons: WorkerAddon[],
     opts: ServeAddonOptions = defaultServeOpts
-): Promise<{ app: express.Application }> => {
+): Promise<{ app: express.Application; listenPromise: Promise<void> }> => {
     const app = express();
     const port = parseInt(<string>process.env.PORT) || 3000;
 
     app.use("/", generateAddonsRouter(addons));
 
-    await new Promise(resolve => {
+    app.use(opts.errorHandler);
+
+    const listenPromise = new Promise<void>(resolve => {
         app.listen(port, resolve);
     });
 
-    return { app };
+    return { app, listenPromise };
 };
