5e3216fe9450f23091890817ed447d79b72af360
Alex0007 <Alex0007lolpvp@gmail.com>
2019-12-14 17:35:27 +0500

    Worker extends Basic

--- src/addons/WorkerAddon.ts
+++ src/addons/WorkerAddon.ts
@@ -13,65 +13,30 @@ import {
 } from "@watchedcom/schema/dist/entities";
 
 import { ActionHandler, IAddon } from "../interfaces";
-import { validateWorkerAddonProps } from "../validators";
+import { validateAddonProps } from "../validators";
 
-type ActionType = WorkerAddonProps["resources"][0]["actions"][0];
+import { BasicActions, BasicAddon } from "./BasicAddon";
 
-export interface ActionsMap {
+export type WorkerActionsMap = BasicActions & {
     directory: ActionHandler<ApiDirectoryRequest, ApiDirectoryResponse>;
     item: ActionHandler<ApiItemRequest, ApiItemResponse>;
     source: ActionHandler<ApiSourceRequest, ApiSourceResponse>;
     subtitle: ActionHandler<ApiSubtitleRequest, ApiSubtitleResponse>;
     resolve: ActionHandler<ApiResolveRequest, ApiResolveResponse>;
-    addon: ActionHandler<any, WorkerAddonProps>;
-}
+};
 
-export class WorkerAddon implements IAddon {
-    private handlersMap: { [action: string]: ActionHandler } = {
-        addon: async () => {
-            return this.getProps();
-        }
-    };
-
-    constructor(private props: WorkerAddonProps) {}
-
-    public getProps() {
-        return this.props;
-    }
-
-    public registerActionHandler<A extends keyof ActionsMap>(
-        action: A,
-        handlerFn: ActionsMap[A]
-    ) {
-        if (this.handlersMap[action]) {
-            throw new Error(
-                `Another handler is already registered for "${action}" action`
-            );
-        }
-        this.handlersMap[action] = handlerFn;
-
-        return this;
-    }
-
-    public unregisterActionHandler(action: keyof ActionsMap) {
-        delete this.handlersMap[action];
-    }
-
-    public getActionHandler(action: string): ActionHandler {
-        const handlerFn = this.handlersMap[action];
-
-        if (!handlerFn) {
-            throw new Error(`No handler for "${action}" action`);
-        }
-
-        return handlerFn;
-    }
-}
+export class WorkerAddon extends BasicAddon<WorkerActionsMap>
+    implements IAddon {}
 
 export const createWorkerAddon = (
     props: Partial<WorkerAddonProps>
 ): WorkerAddon => {
-    const addonProps = validateWorkerAddonProps({ ...props, type: "worker" });
+    const addonProps = validateAddonProps<WorkerAddonProps>({
+        ...props,
+        type: "worker"
+    });
     const addon = new WorkerAddon(addonProps);
     return addon;
 };
+
+// export const createWorkerAddon = addonFn<WorkerAddonProps,WorkerAddon>({AddonClass: WorkerAddon, type: 'worker'})
