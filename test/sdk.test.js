0d68be7c933397dbc35da276f9c0a2e63887366f
MediaURL <contact@mediaurl.io>
2019-12-17 15:55:16 +0100

    Fixed test case

--- test/sdk.test.js
+++ test/sdk.test.js
@@ -1,14 +1,11 @@
 const Sdk = require("../");
 
 const exported = [
-    "start",
-    "startServer",
-    "startCli",
-    "Addon",
-    "createAddon",
-    "createRepository",
-    "getCache",
-    "setCache"
+    "createWorkerAddon",
+    "createRepositoryAddon",
+    "createBundleAddon",
+    "generateRouter",
+    "serveAddons"
 ];
 
 test("SDK should export all needed methods and properties", () => {
