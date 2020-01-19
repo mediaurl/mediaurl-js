e9818de866d22289bafc723f498ecdae2297cb5f
Alex0007 <Alex0007lolpvp@gmail.com>
2020-01-20 04:30:27 +0500

    ts project bootstrap

--- cli/index.js
+++ cli/index.js
@@ -3,51 +3,145 @@ const program = require("commander");
 const fork = require("child_process").fork;
 const path = require("path");
 const { guessTsMain } = require("guess-ts-main");
+const inquirer = require("inquirer");
+const fs = require("fs-extra");
+
+const { executeProjectTemplate, tsProject } = require("./templates");
 
 const startScriptPath = path.resolve(__dirname, "start");
 
 const cwd = process.cwd();
 
+const startHandler = (files, cmdObj) => {
+    let tsConfig = null;
+
+    try {
+        tsConfig = require(path.resolve(cwd, "tsconfig.json"));
+    } catch {}
+
+    if (cmdObj.prod && files.length === 0) {
+        files.push(cwd);
+    }
+
+    // It's a ts project and we want to start ts version instead
+    if (tsConfig && files.length === 0) {
+        files.push(guessTsMain(cwd));
+    }
+
+    console.log({ "Serving addons": files, "Live reload": !cmdObj.prod });
+
+    const execPath = path.resolve(cwd, "node_modules", ".bin", "ts-node-dev");
+
+    return fork(
+        startScriptPath,
+        files,
+        cmdObj.prod
+            ? undefined
+            : {
+                  execPath,
+                  execArgv: ["--no-notify", "--transpileOnly"]
+              }
+    );
+};
+
+const createHandler = async (folderName, cmdObj) => {
+    const { template, force } = cmdObj;
+    console.log("inside createHandler", folderName, { template, force });
+
+    const addonPath = path.resolve(process.cwd(), folderName);
+
+    const folderExists = await fs.pathExists(addonPath);
+
+    if (folderExists) {
+        throw new Error("Folder already exists");
+    }
+
+    const defaults = {
+        name: folderName,
+        actions: ["directory", "item"],
+        itemTypes: ["movie", "series"]
+    };
+
+    const userInput = await inquirer
+        .prompt(
+            [
+                template
+                    ? null
+                    : {
+                          type: "list",
+                          choices: [
+                              { name: "TypeScript", value: "ts" },
+                              { name: "JavaScript", value: "js" }
+                          ],
+                          name: "template",
+                          default: 0
+                      },
+                ...(force
+                    ? []
+                    : [
+                          {
+                              name: "name",
+                              type: "input",
+                              default: folderName
+                          },
+                          {
+                              name: "actions",
+                              type: "checkbox",
+                              choices: [
+                                  "directory",
+                                  "item",
+                                  "source",
+                                  "subtitle",
+                                  "resolve"
+                              ],
+                              default: ["directory", "item"]
+                          },
+                          {
+                              name: "itemTypes",
+                              type: "checkbox",
+                              choices: [
+                                  "movie",
+                                  "series",
+                                  "directory",
+                                  "channel",
+                                  "iptv"
+                              ],
+                              default: ["movie", "series"]
+                          },
+                          {
+                              name: "requestArgs",
+                              type: "checkbox",
+                              choices: ["imdb_id", "tmdb_id"],
+                              default: ["imdb_id", "tmdb_id"],
+                              when: ({ actions }) =>
+                                  actions.indexOf("source") !== -1
+                          }
+                      ])
+            ].filter(_ => _)
+        )
+        .then(responses => ({ template, ...defaults, ...responses }));
+
+    if (userInput.template !== "ts") {
+        throw new Error("Only TS projects supported for now");
+    }
+
+    console.log(userInput);
+
+    await executeProjectTemplate(tsProject, addonPath, userInput);
+};
+
 program
-    .option("--prod", "Start the server in production mode")
     .command("start [files...]")
+    .option("--prod", "Start the server in production mode")
     .description("Start the WATCHED SDK server")
-    .action(files => {
-        let tsConfig = null;
+    .action(startHandler);
 
-        try {
-            tsConfig = require(path.resolve(cwd, "tsconfig.json"));
-        } catch {}
-
-        if (program.prod && files.length === 0) {
-            files.push(cwd);
-        }
-
-        // It's a ts project and we want to start ts version instead
-        if (tsConfig && files.length === 0) {
-            files.push(guessTsMain(cwd));
-        }
-
-        console.log({ "Serving addons": files, "Live reload": !program.prod });
-
-        const execPath = path.resolve(
-            cwd,
-            "node_modules",
-            ".bin",
-            "ts-node-dev"
-        );
-
-        return fork(
-            startScriptPath,
-            files,
-            program.prod
-                ? undefined
-                : {
-                      execPath,
-                      execArgv: ["--no-notify", "--transpileOnly"]
-                  }
-        );
-    });
+program
+    .command("create <name>")
+    .description("Create WATCHED addon folder")
+    .option("--template <template>", "js|ts")
+    .option("--force")
+    .action(createHandler);
 
 program.on("command:*", function() {
     program.outputHelp();
---
+++ cli/templates.js
@@ -0,0 +1,125 @@
+const path = require("path");
+const fs = require("fs-extra");
+
+const executeProjectTemplate = async (template, basePath, input) => {
+    for (const filePath of Object.keys(template)) {
+        const targetPath = path.resolve(basePath, ...filePath.split("/"));
+        const data = template[filePath];
+        await fs.outputFile(
+            targetPath,
+            typeof data === "function" ? data(input) : data
+        );
+    }
+};
+
+const tsConfigJson = `{
+    "compilerOptions": {
+        "target": "es5",
+        "strictNullChecks": true,
+        "outDir": "dist",
+    },
+    "include": ["src/index.ts"]
+}`;
+
+const indexTs = ({ name, actions, itemTypes, requestArgs }) => {
+    const addonVar = `${name}Addon`;
+
+    const handlersBlock = actions
+        .map(action => {
+            return `${addonVar}.registerActionHandler("${action}", ${action}Handler);`;
+        })
+        .join("\n\n");
+
+    const addonProps = {
+        id: name,
+        version: "1.0.0",
+        name: name,
+        flags: {
+            adult: false
+        },
+        requestArgs,
+        actions,
+        itemTypes
+    };
+
+    const content = `import { createWorkerAddon } from "@watchedcom/sdk";
+
+import { ${actions
+        .map(action => `${action}Handler`)
+        .join(", ")} } from "./handlers";
+
+export const ${addonVar} = createWorkerAddon(${JSON.stringify(
+        addonProps,
+        null,
+        4
+    )});
+
+${handlersBlock}`;
+
+    return content;
+};
+
+const handlersTs = ({ actions }) => {
+    const handlersBlock = actions
+        .map(
+            action =>
+                `export const ${action}Handler: WorkerHandlers["${action}"] = async (input, ctx) => {
+    // ${action} action handler code goes here
+    throw new Error("Not implemented");
+};`
+        )
+        .join("\n\n");
+    const content = `import { WorkerHandlers } from "@watchedcom/sdk";
+
+${handlersBlock}
+`;
+    return content;
+};
+
+const envExample = `# Used in development
+# Rename to .env and set your vars
+NODE_ENV=development`;
+
+const readme = ({ name }) => {
+    const content = `# ${name} addon for WATCHED.com
+
+\`\`\`shell
+npm i
+npm run develop
+\`\`\`
+`;
+
+    return content;
+};
+
+const tsProject = {
+    "tsconfig.json": tsConfigJson,
+    "package.json": ({ name }) =>
+        JSON.stringify(
+            {
+                name: "addon-" + name,
+                version: "1.0.0",
+                main: "dist",
+                scripts: {
+                    build: "tsc -p .",
+                    start: "watched-sdk start --prod",
+                    develop: "watched-sdk start"
+                },
+                dependencies: {
+                    "@watchedcom/sdk": "latest"
+                },
+                devDependencies: {
+                    typescript: "latest"
+                }
+            },
+            null,
+            2
+        ),
+    ".gitignore": ["node_modules", ".env", "dist"].join("\n"),
+    "src/index.ts": indexTs,
+    "src/handlers.ts": handlersTs,
+    ".env.example": envExample,
+    "README.md": readme
+};
+
+module.exports = { executeProjectTemplate, tsProject };
