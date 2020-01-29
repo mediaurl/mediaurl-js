ced938006ca68e46e117bfcfb9e41caa0f501f42
MediaURL <contact@mediaurl.io>
2020-01-30 00:16:02 +0100

    Again missing indents

--- src/error-handler.ts
+++ src/error-handler.ts
@@ -4,14 +4,14 @@ import * as express from "express";
  * Error handler used by default. Can be overriden
  */
 export const errorHandler: express.ErrorRequestHandler = (
-    error,
-    req,
-    res,
-    next
+  error,
+  req,
+  res,
+  next
 ) => {
-    console.error(error);
-    res.status(error.statusCode || 500).send({
-        error: error.message || error,
-        stack: process.env.NODE_ENV === "development" ? error.stack : undefined
-    });
+  console.error(error);
+  res.status(error.statusCode || 500).send({
+    error: error.message || error,
+    stack: process.env.NODE_ENV === "development" ? error.stack : undefined
+  });
 };
--- test/sdk.test.js
+++ test/sdk.test.js
@@ -1,13 +1,13 @@
 const Sdk = require("../");
 
 const exported = [
-    "createWorkerAddon",
-    "createRepositoryAddon",
-    "createBundleAddon",
-    "createRouter",
-    "serveAddons"
+  "createWorkerAddon",
+  "createRepositoryAddon",
+  "createBundleAddon",
+  "createRouter",
+  "serveAddons"
 ];
 
 test("SDK should export all needed methods and properties", () => {
-    expect(exported.every(key => Sdk[key])).toBeTruthy();
+  expect(exported.every(key => Sdk[key])).toBeTruthy();
 });
