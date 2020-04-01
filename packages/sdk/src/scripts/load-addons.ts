6af26d764e9c5c0a8b88e867d85c1bd411b2b1f2
Alex0007 <Alex0007lolpvp@gmail.com>
2020-04-02 04:29:44 +0500

    don't stuck in cases where Infinite promise is exported

--- packages/sdk/src/scripts/load-addons.ts
+++ packages/sdk/src/scripts/load-addons.ts
@@ -1,46 +1,52 @@
-import { uniqBy } from "lodash";
 import * as path from "path";
 import { BasicAddonClass } from "../addons";
 
-const requireAddons = async (pathStr: string) => {
+const isAddon = (item: any): item is BasicAddonClass => {
+  try {
+    item.getProps();
+    item.getType();
+    item.getId();
+    return true;
+  } catch {}
+  return false;
+};
+
+const _filterAddons = async (
+  exportedValues: any[]
+): Promise<BasicAddonClass[]> => {
+  const result: (BasicAddonClass | BasicAddonClass[])[] = [];
+
+  for (const _ of exportedValues) {
+    const item = await Promise.race([
+      _,
+      new Promise((_, reject) => {
+        setTimeout(() => {
+          reject(
+            new Error(
+              "Unable to resolve exported value for a long period of time"
+            )
+          );
+        }, 15000);
+      })
+    ]);
+
+    if (Array.isArray(item)) {
+      result.push(await _filterAddons(item));
+    }
+
+    if (isAddon(item)) {
+      result.push(item);
+    }
+  }
+  return result.flat();
+};
+
+const _requireAddons = async (pathStr: string) => {
   const requiredFile = require(pathStr);
   const sources = { default: requiredFile, ...requiredFile };
 
-  const addons: BasicAddonClass[] = [];
-  const keys = Object.keys(sources);
+  const addons = await _filterAddons(Object.values(sources));
 
-  const handleArray = (key: string, value: any) => {
-    if (!Array.isArray(value)) return false;
-    for (let i = 0; i < value.length; i++) {
-      const k = `${key}_` + i;
-      sources[k] = value[i];
-      keys.push(k);
-    }
-    return true;
-  };
-
-  while (keys.length > 0) {
-    const key = <string>keys.shift();
-    const source = sources[key];
-    try {
-      // Make sure it's a WATCHED addon
-      source.getProps();
-      source.getType();
-      source.getId();
-      addons.push(source);
-    } catch (e) {
-      if (typeof source?.then === "function") {
-        console.info(`Resolving exported promise "${key}"`);
-        const value = await Promise.resolve(source);
-        if (!handleArray(key, value)) {
-          sources[key] = value;
-          keys.push(key);
-        }
-      } else {
-        handleArray(key, source);
-      }
-    }
-  }
   if (addons.length === 0) {
     throw new Error(
       `Script "${pathStr}" does not export any valid WATCHED addons.`
@@ -53,9 +59,8 @@ const requireAddons = async (pathStr: string) => {
 export const loadAddons = async (files: string[]) => {
   if (files.length === 0) files.push(".");
   const cwd = process.cwd();
-  let addons: BasicAddonClass[] = [];
-  for (const file of files) {
-    addons = [...addons, ...(await requireAddons(path.resolve(cwd, file)))];
-  }
-  return uniqBy(addons, addon => addon);
+
+  return Promise.all(
+    files.map(file => path.resolve(cwd, file)).map(_requireAddons)
+  ).then(_ => _.flat());
 };
